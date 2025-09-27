bf01399590916a0ed33f061a0f39aad69477583f
dandelionn <michea.paul@yahoo.com>
2025-09-27 19:56:45 +0300

    feat: display message after account is verified

--- src/containers/verify-account/verify-account.tsx
+++ src/containers/verify-account/verify-account.tsx
@@ -2,25 +2,31 @@
 
 import VerifyAccount from '../../components/auth/verify-account';
 import { useEffect } from 'react';
-import { useParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
 import { getCsrfToken, VerifyTokenRequest } from '@/api/generated';
 import { postAuthVerifySignupMutation } from '@/api/generated/@tanstack/react-query.gen';
+import AccountVerified from '@/components/auth/account-verified';
 
 export default function VerifyAccountContainer() {
-	const { token } = useParams<{ token: string }>();
+	const searchParams = useSearchParams();
+	const token = searchParams.get('token');
 	const { data, error, mutate, status, isPending } = useMutation({
 		...postAuthVerifySignupMutation(),
 	});
+	console.log('token', token);
 
 	useEffect(() => {
-		verify({ token: token });
+		console.log('token', token);
+		if (token) {
+			verify({ token });
+		}
 	}, [token]);
 
 	const verify = async (data: VerifyTokenRequest) => {
 		mutate({
 			headers: {
-				'x-csrf-token': (await getCsrfToken({ cache: 'no-store' })).data?.csrfToken,
+				'x-csrf-token': (await getCsrfToken({ cache: 'no-store' })).data?.csrfToken as string,
 			},
 			body: {
 				...data,
@@ -30,7 +36,11 @@ export default function VerifyAccountContainer() {
 
 	return (
 		<>
-			<VerifyAccount messages={error?.messages} />
+			{status === 'success' ? (
+				<AccountVerified />
+			) : (
+				<VerifyAccount messages={error?.messages} loading={isPending} />
+			)}
 		</>
 	);
 }
